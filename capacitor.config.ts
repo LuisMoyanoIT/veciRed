4845f0047ae0c9c08d27831e09a6f662bc070394
LuisMoyanoIT <luismocru@gmail.com>
2022-06-05 18:17:10 -0500

    configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  //appId: 'io.ionic.starter',
+  appId: 'com.ubb.veciRed',
+  appName: 'veciRed',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
